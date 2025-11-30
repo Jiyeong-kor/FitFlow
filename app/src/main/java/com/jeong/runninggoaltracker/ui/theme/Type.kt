fb267343da0bc14d1e02a9be0093bc7f26e4c0be
Jiyeong <jeongjiyeong486@gmail.com>
2025-11-30 18:24:08 +0900

    refactor: Color.kt, Theme.kt, Type.kt 업데이트

--- app/src/main/java/com/jeong/runninggoaltracker/ui/theme/Type.kt
+++ app/src/main/java/com/jeong/runninggoaltracker/ui/theme/Type.kt
@@ -2,33 +2,58 @@ package com.jeong.runninggoaltracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jeong.runninggoaltracker.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),               // 100
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),   // 200
+    Font(R.font.pretendard_light, FontWeight.Light),             // 300
+    Font(R.font.pretendard_regular, FontWeight.Normal),          // 400
+    Font(R.font.pretendard_medium, FontWeight.Medium),           // 500
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),       // 600
+    Font(R.font.pretendard_bold, FontWeight.Bold),               // 700
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),     // 800
+    Font(R.font.pretendard_black, FontWeight.Black)              // 900
+)
+
+val AppTypography = Typography(
+    // 화면 큰 타이틀
+    titleLarge = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp
+    ),
+    // 중간 타이틀
+    titleMedium = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+    // 일반 본문
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 22.sp
     ),
+    // 보조 본문
+    bodyMedium = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    // 라벨/버튼 등 작은 글자
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp,
+        lineHeight = 16.sp
     )
-    */
 )
