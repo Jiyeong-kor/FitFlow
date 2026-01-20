4d8adda02b16be44bff6426122922eb6fbb07569
Jiyeong <jeongjiyeong486@gmail.com>
2026-01-20 17:33:40 +0900

    fix(running-service): 트래커 시작 시 COARSE 위치 권한도 허용하도록 보완

--- feature/record/src/main/java/com/jeong/runninggoaltracker/feature/record/tracking/RunningTrackerManager.kt
+++ feature/record/src/main/java/com/jeong/runninggoaltracker/feature/record/tracking/RunningTrackerManager.kt
@@ -38,5 +38,9 @@ class RunningTrackerManager @Inject constructor(
         ContextCompat.checkSelfPermission(
             context,
             Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
 }
