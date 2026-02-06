0fb814518fb4375bd1200afb3833175ac8be17e2
Jiyeong <jeongjiyeong486@gmail.com>
2026-02-06 19:12:09 +0900

    feat(designsystem): 커스텀 아이콘 에셋 추가 및 AppIcons 객체 구현

---
+++ shared/designsystem/src/main/java/com/jeong/runninggoaltracker/shared/designsystem/icon/AppIcons.kt
@@ -0,0 +1,86 @@
+package com.jeong.runninggoaltracker.shared.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.jeong.runninggoaltracker.shared.designsystem.R
+
+object AppIcons {
+    @Composable
+    fun accountCircle(): Painter = painterResource(R.drawable.ic_account_circle)
+
+    @Composable
+    fun add(): Painter = painterResource(R.drawable.ic_add)
+
+    @Composable
+    fun arrowBack(): Painter = painterResource(R.drawable.ic_arrow_back)
+
+    @Composable
+    fun arrowForward(): Painter = painterResource(R.drawable.ic_arrow_forward)
+
+    @Composable
+    fun calendar(): Painter = painterResource(R.drawable.ic_calendar_month)
+
+    @Composable
+    fun check(): Painter = painterResource(R.drawable.ic_check)
+
+    @Composable
+    fun close(): Painter = painterResource(R.drawable.ic_close)
+
+    @Composable
+    fun delete(): Painter = painterResource(R.drawable.ic_delete)
+
+    @Composable
+    fun description(): Painter = painterResource(R.drawable.ic_description)
+
+    @Composable
+    fun directionsRun(): Painter = painterResource(R.drawable.ic_directions_run)
+
+    @Composable
+    fun directionsRunOutlined(): Painter = painterResource(R.drawable.ic_directions_run_outlined)
+
+    @Composable
+    fun directionsWalk(): Painter = painterResource(R.drawable.ic_directions_walk)
+
+    @Composable
+    fun edit(): Painter = painterResource(R.drawable.ic_edit)
+
+    @Composable
+    fun fitnessCenter(): Painter = painterResource(R.drawable.ic_fitness_center)
+
+    @Composable
+    fun home(): Painter = painterResource(R.drawable.ic_home)
+
+    @Composable
+    fun keyboardArrowRight(): Painter = painterResource(R.drawable.ic_keyboard_arrow_right)
+
+    @Composable
+    fun locationOn(): Painter = painterResource(R.drawable.ic_location_on)
+
+    @Composable
+    fun notifications(): Painter = painterResource(R.drawable.ic_notifications)
+
+    @Composable
+    fun notificationsOutlined(): Painter = painterResource(R.drawable.ic_notifications_outlined)
+
+    @Composable
+    fun pause(): Painter = painterResource(R.drawable.ic_pause)
+
+    @Composable
+    fun person(): Painter = painterResource(R.drawable.ic_person)
+
+    @Composable
+    fun photoCamera(): Painter = painterResource(R.drawable.ic_photo_camera)
+
+    @Composable
+    fun playArrow(): Painter = painterResource(R.drawable.ic_play_arrow)
+
+    @Composable
+    fun refresh(): Painter = painterResource(R.drawable.ic_refresh)
+
+    @Composable
+    fun remove(): Painter = painterResource(R.drawable.ic_remove)
+
+    @Composable
+    fun stop(): Painter = painterResource(R.drawable.ic_stop)
+}
