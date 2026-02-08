314db068ba04a4a938ad9af404741c52409ee3cc
Jiyeong <jeongjiyeong486@gmail.com>
2026-02-08 22:58:10 +0900

    build: 사용하지 않는 coroutines-test 의존성 제거

--- feature/record/build.gradle.kts
+++ feature/record/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     ksp(libs.hilt.compiler)
 
     testImplementation(libs.bundles.test.unit)
-    testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.bundles.androidx.compose.test)
