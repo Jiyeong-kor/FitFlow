e29dea2cacf954283a01246b51ad9616ed2dadba
Jiyeong <jeongjiyeong486@gmail.com>
2026-02-19 20:17:16 +0900

    test(auth): 온보딩 플로우 테스트 내 미사용 임포트 제거
    - 코드 가독성 향상을 위해 사용되지 않는 assertExists 임포트 삭제

--- feature/auth/src/androidTest/java/com/jeong/runninggoaltracker/feature/auth/presentation/OnboardingFlowTest.kt
+++ feature/auth/src/androidTest/java/com/jeong/runninggoaltracker/feature/auth/presentation/OnboardingFlowTest.kt
@@ -1,7 +1,6 @@
 package com.jeong.runninggoaltracker.feature.auth.presentation
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
